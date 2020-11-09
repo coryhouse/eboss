88d87f0ccbe1c8c1589dd32d6ce89b284db9d332
Cory House <housecor@gmail.com>
2020-11-09 14:17:38 -0600

    Switch to async await

--- src/api/candidatesApi.js
+++ src/api/candidatesApi.js
@@ -1,7 +1,6 @@
 // Return a promise that ultimately resolves to an array of candidates
-export function getCandidates() {
-  return fetch("http://localhost:3001/candidates").then((response) => {
-    if (!response.ok) throw response;
-    return response.json();
-  });
+export async function getCandidates() {
+  const response = await fetch("http://localhost:3001/candidates");
+  if (!response.ok) throw response;
+  return response.json(); // this returns a promise.
 }
