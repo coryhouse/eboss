d752891fdbc7ed9ba2ff18dcf19f13d2cdb77aea
Cory House <housecor@gmail.com>
2020-11-09 11:58:29 -0600

    Switch to table

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
+/**
+ * 1. Get candidates from mock API
+ * 2. Show a loading indicator (spinner)
+ * 3. Handle loading errors
+ * 4. Create voting page
+ * 4. Configure routes between pages
+ * 5. Vote on candidate
+ * 6. Display votes on candidates page
+ * 7. Styling
+ * 8. Using reusable components
+ * 9. Keys
+ * 10. propTypes
+ * 11. Testing
+ *
+ */
+
 import React from "react";
 
 export const App = () => {
@@ -10,16 +26,25 @@ export const App = () => {
   // The candidate argument is automatically injected by the map function.
   function renderCandidate(candidate) {
     return (
-      <li>
-        {candidate.name} - {candidate.rank}
-      </li>
+      <tr>
+        <td>{candidate.name}</td>
+        <td>{candidate.rank}</td>
+      </tr>
     );
   }
 
   return (
-    <>
+    <ul>
       <h1>Candidates</h1>
-      <ul>{candidates.map(renderCandidate)}</ul>
-    </>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rank</th>
+          </tr>
+        </thead>
+        <tbody>{candidates.map(renderCandidate)}</tbody>
+      </table>
+    </ul>
   );
 };
