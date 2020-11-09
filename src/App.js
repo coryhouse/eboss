c704bdbd18a64bfade823b9ed52a2ceede556f55
Cory House <housecor@gmail.com>
2020-11-09 11:25:58 -0600

    Display candidates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import React from "react";
 
 export const App = () => {
-  return <h2>My component</h2>;
+  const candidates = [
+    { name: "Cory", rank: "Airman" },
+    { name: "David", rank: "General" },
+    { name: "Ann", rank: "Tech Sargent" },
+  ];
+
+  // The candidate argument is automatically injected by the map function.
+  function renderCandidate(candidate) {
+    return (
+      <li>
+        {candidate.name} - {candidate.rank}
+      </li>
+    );
+  }
+
+  return (
+    <>
+      <h1>Candidates</h1>
+      <ul>{candidates.map(renderCandidate)}</ul>
+    </>
+  );
 };
