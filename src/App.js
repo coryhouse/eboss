dd3cb1c07c56ed949841f05071be519a3e5bc6b3
Cory House <housecor@gmail.com>
2020-11-09 15:02:32 -0600

    Add nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { Candidates } from "./Candidates";
 import { CandidateDetails } from "./CandidateDetails";
+import { Route, Link } from "react-router-dom";
 
 export function App() {
   return (
     <>
-      <Candidates />
-      <CandidateDetails />
+      <Link to="/">Home</Link> | <Link to="/candidates">Candidates</Link>
+      <Route path="/" exact>
+        <h1>Home</h1>
+      </Route>
+      <Route path="/candidates">
+        <Candidates />
+      </Route>
+      <Route path="/details">
+        <CandidateDetails />
+      </Route>
     </>
   );
 }
