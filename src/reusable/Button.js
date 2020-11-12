a955fbeb51f9cf08b7caed0dbd4e1c94a1fe219c
Cory House <housecor@gmail.com>
2020-11-12 14:38:30 -0600

    tweak

--- src/reusable/Button.js
+++ src/reusable/Button.js
@@ -3,11 +3,7 @@ import styles from "./Button.module.css";
 
 export function Button(props) {
   return (
-    <button
-      className={`${styles.root} ${props.className}`}
-      style={props.style}
-      id={props.id}
-    >
+    <button className={styles.root} style={props.style} id={props.id}>
       {props.children}
     </button>
   );
