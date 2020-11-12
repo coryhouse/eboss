fef4f15f3df43da93b3e3eb70c9a1d24bba2c8be
Cory House <housecor@gmail.com>
2020-11-12 14:58:07 -0600

    Add propTypes

--- src/reusable/Button.js
+++ src/reusable/Button.js
@@ -1,10 +1,23 @@
 import React from "react";
 import styles from "./Button.module.css";
+import PropTypes from "prop-types";
 
-export function Button(props) {
+export default function Button(props) {
   return (
     <button className={styles.root} style={props.style} id={props.id}>
       {props.children}
     </button>
   );
 }
+
+Button.propTypes = {
+  style: PropTypes.object,
+  children: PropTypes.any.isRequired,
+  /** HTML ID to apply to the button
+   *
+   * - item one
+   * - item two
+   * - [more info here](http://google.com)
+   */
+  id: PropTypes.string,
+};
