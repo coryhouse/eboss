b5800d7a545b4c33ae6d7d36e80cb05e25f3425f
Cory House <housecor@gmail.com>
2020-11-13 14:52:55 -0600

    Config store and provider

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App"; // Named import
 import { BrowserRouter } from "react-router-dom";
+import configureStore from "./redux/configureStore";
+import { Provider } from "react-redux";
+
+const store = configureStore();
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
