79033606dc160cf9033a7b62300d4c60cd89fb5d
Cory House <housecor@gmail.com>
2020-11-13 13:43:16 -0600

    wrote unit tests

--- src/utils/candidateUtils.test.js
+++ src/utils/candidateUtils.test.js
@@ -1,40 +1,90 @@
-import { getCandidateWithAverageScore } from "./candidateUtils";
+import {
+  getCandidateWithAverageScore,
+  getAverageFromArray,
+} from "./candidateUtils";
 
-it("should return a candidate object that includes the candidates average score", () => {
-  // Arrange
-  const candidate = {
-    id: 1,
-    name: "Cory",
-    rank: "Airman",
-  };
+describe("getAverageFromArray", () => {
+  it("should return the value passed if the array has a single value", () => {
+    expect(getAverageFromArray([3])).toEqual(3);
+  });
+});
 
-  const scores = [
-    {
-      id: 3,
-      candidateId: 3,
-      value: 10,
-    },
-    {
-      candidateId: 1,
-      score: 6,
-      id: 4,
-    },
-    {
-      candidateId: 1,
-      score: 7,
-      id: 5,
-    },
-  ];
+describe("getCandidateWithAverageScore", () => {
+  it("should return a candidate object that includes the candidates average score", () => {
+    // Arrange
+    const candidate = {
+      id: 1,
+      name: "Cory",
+      rank: "Airman",
+    };
 
-  // Act
-  const result = getCandidateWithAverageScore(candidate, scores);
+    const scores = [
+      {
+        id: 3,
+        candidateId: 3,
+        value: 10,
+      },
+      {
+        candidateId: 1,
+        score: 6,
+        id: 4,
+      },
+      {
+        candidateId: 1,
+        score: 7,
+        id: 5,
+      },
+    ];
 
-  // Assert
-  const expected = {
-    id: 1,
-    name: "Cory",
-    rank: "Airman",
-    averageScore: 6.5,
-  };
-  expect(result).toEqual(expected);
+    // Act
+    const result = getCandidateWithAverageScore(candidate, scores);
+
+    // Assert
+    const expected = {
+      id: 1,
+      name: "Cory",
+      rank: "Airman",
+      averageScore: 6.5,
+    };
+    expect(result).toEqual(expected);
+  });
+
+  it("should return a candidate object that includes a null score if no scores exist yet", () => {
+    // Arrange
+    const candidate = {
+      id: 2,
+      name: "Cory",
+      rank: "Airman",
+    };
+
+    const scores = [
+      {
+        id: 3,
+        candidateId: 3,
+        value: 10,
+      },
+      {
+        candidateId: 1,
+        score: 6,
+        id: 4,
+      },
+      {
+        candidateId: 1,
+        score: 7,
+        id: 5,
+      },
+    ];
+
+    // Act
+    const result = getCandidateWithAverageScore(candidate, scores);
+
+    // Assert
+    const expected = {
+      id: 2,
+      name: "Cory",
+      rank: "Airman",
+      averageScore: null,
+    };
+    expect(result).toEqual(expected);
+  });
 });
