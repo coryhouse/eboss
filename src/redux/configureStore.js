99c73d1860dd520722d829c4fec151bfe5ad7e23
Cory House <housecor@gmail.com>
2020-11-13 14:53:02 -0600

    Config store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,9 @@
+import { createStore, compose } from "redux";
+import rootReducer from "./reducers";
+
+export default function configureStore(initialState) {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const store = createStore(rootReducer, initialState, composeEnhancers());
+  return store;
+}
