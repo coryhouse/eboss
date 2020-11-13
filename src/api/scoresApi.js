8c8fc18fb86c671698eb19e1d8ea59b5a440c280
Cory House <housecor@gmail.com>
2020-11-13 11:29:18 -0600

    add api

---
+++ src/api/scoresApi.js
@@ -0,0 +1,11 @@
+export async function saveScore(candidateId, score) {
+  const response = await fetch("http://localhost:3001/score", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ candidateId, score }),
+  });
+  if (!response.ok) throw response;
+  return response.json(); // this returns a promise.
+}
